246416685373a4b6d529f7136d2b90907df61443
SyBr <sybr@sybrcq>
2016-12-19 13:22:33 +0100

    Nou mapa. 2016.

---
+++ 2016.mm
@@ -0,0 +1,28 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1482149608170" ID="ID_29907350" MODIFIED="1482149620552" TEXT="2016">
+<node CREATED="1482149621367" ID="ID_1391873210" MODIFIED="1482149626549" POSITION="right" TEXT="DESEMBRE">
+<node CREATED="1482149658942" ID="ID_1096773859" MODIFIED="1482149688045" TEXT="19DILLUNS">
+<node CREATED="1482149703399" ID="ID_254155864" MODIFIED="1482149738082" TEXT="FETS"/>
+<node CREATED="1482149711300" ID="ID_468004220" MODIFIED="1482149732156" TEXT="OBJECTIUS">
+<node CREATED="1482149741066" ID="ID_1270941850" MODIFIED="1482150047609" TEXT="Exercicis EXTRA Tuto React">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Aquests exercicis no resolts tenen molt mala bava. Impliquen canvis profunds en l'estructura i m'est&#224;n apareguent molts problemes il&#183;l&#250;sament inesperats.
+    </p>
+    <p>
+      Array.concat() no fa el que hauria de fer amb l'actualitzaci&#243; de l'estat... en fi. Continue. Porte ja no s&#233; quants dies amb aquesta xorrada... per&#242; suposse que aix&#237; &#233;s com s'apren.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
