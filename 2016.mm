a9d84508501dccbb6a2b7514c0c6b5f56dc81883
SyBr <sybr@sybrcq>
2016-12-20 02:54:49 +0100

    Aprenent sobre Immutable.js...

--- 2016.mm
+++ 2016.mm
@@ -3,7 +3,27 @@
 <node CREATED="1482149608170" ID="ID_29907350" MODIFIED="1482149620552" TEXT="2016">
 <node CREATED="1482149621367" ID="ID_1391873210" MODIFIED="1482149626549" POSITION="right" TEXT="DESEMBRE">
 <node CREATED="1482149658942" ID="ID_1096773859" MODIFIED="1482149688045" TEXT="19DILLUNS">
-<node CREATED="1482149703399" ID="ID_254155864" MODIFIED="1482149738082" TEXT="FETS"/>
+<node CREATED="1482149703399" ID="ID_254155864" MODIFIED="1482149738082" TEXT="FETS">
+<node CREATED="1482197931922" ID="ID_1261535768" MODIFIED="1482198738934" TEXT="Coses apreses">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      No hem acabat els exercicis Extra... com era d'esperar, per&#242; he apr&#233;s algunes coses noves. I &#233;s clar que si vaig a gastar JS de forma seriosa, haur&#233; de dominar-lo molt m&#233;s del que ho faig ara. I necessitar&#233; fer-ho amb bon nivell de detall. &#201;s per aix&#242; que m'he encabotat en fer funcionar aquests exercicis. Pel cam&#237; he anat aprenent sobre Map, Slice, Reduce i altres funcions &#250;tils de JS. Avu&#237; he descobert Object.is() i he comen&#231;at a investigar Immutable.js. Sembla interessant. Vegent la confer&#232;ncia de presentaci&#243; de Lee Byron a OREDEV, m'ha refrescat el concepte de composici&#243; funcional amb l'exemple de la memoitzaci&#243;. Quant de potencial...
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      No vull parar.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
 <node CREATED="1482149711300" ID="ID_468004220" MODIFIED="1482149732156" TEXT="OBJECTIUS">
 <node CREATED="1482149741066" ID="ID_1270941850" MODIFIED="1482150047609" TEXT="Exercicis EXTRA Tuto React">
 <richcontent TYPE="NOTE"><html>
