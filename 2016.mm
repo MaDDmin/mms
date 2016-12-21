af763fc6aafe3f39a9c930f0a3e6d0a474cc711c
SyBr <sybr@sybrcq>
2016-12-21 15:00:41 +0100

    A seguir...

--- 2016.mm
+++ 2016.mm
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1482149608170" ID="ID_29907350" MODIFIED="1482149620552" TEXT="2016">
@@ -15,38 +14,25 @@
       No hem acabat els exercicis Extra... com era d'esperar, per&#242; he apr&#233;s algunes coses noves. I &#233;s clar que si vaig a gastar JS de forma seriosa, haur&#233; de dominar-lo molt m&#233;s del que ho faig ara. I necessitar&#233; fer-ho amb bon nivell de detall. &#201;s per aix&#242; que m'he encabotat en fer funcionar aquests exercicis. Pel cam&#237; he anat aprenent sobre Map, Slice, Reduce i altres funcions &#250;tils de JS. Avu&#237; he descobert Object.is() i he comen&#231;at a investigar Immutable.js. Sembla interessant. Vegent la confer&#232;ncia de presentaci&#243; de Lee Byron a OREDEV, m'ha refrescat el concepte de composici&#243; funcional amb l'exemple de la memoitzaci&#243;. Quant de potencial...
     </p>
     <p>
-=======
-<map version="0.9.0" NAME="2016" DATE="1482264712000" FILE="2016.mm" MAPID="wppSNSlvR8W6U6GzW9wZsA==" BGCOLOR="#F2F3F7">
-<node ID="ID_29907350" TEXT="2016" COLOR="#000034" TSGSTYLE="NodeMasterGraphic">
-<font NAME="SansSerif" SIZE="16" />
-<edge COLOR="#309EFF" />
-<node ID="ID_1391873210" TEXT="DESEMBRE" POSITION="right" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
-<edge COLOR="#309EFF" />
-<node ID="ID_1096773859" TEXT="19DILLUNS" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
-<edge COLOR="#309EFF" />
-<node ID="ID_254155864" TEXT="FETS" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
-<edge COLOR="#309EFF" />
-<node ID="ID_1261535768" TEXT="Coses apreses" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
-<edge COLOR="#309EFF" />
-<richcontent TYPE="NOTE">
-<pre>No hem acabat els exercicis Extra... com era d'esperar, per&#242; he apr&#233;s algunes coses noves. I &#233;s clar que si vaig a gastar JS de forma seriosa, haur&#233; de dominar-lo molt m&#233;s del que ho faig ara. I necessitar&#233; fer-ho amb bon nivell de detall. &#201;s per aix&#242; que m'he encabotat en fer funcionar aquests exercicis. Pel cam&#237; he anat aprenent sobre Map, Slice, Reduce i altres funcions &#250;tils de JS. Avu&#237; he descobert Object.is() i he comen&#231;at a investigar Immutable.js. Sembla interessant. Vegent la confer&#232;ncia de presentaci&#243; de Lee Byron a OREDEV, m'ha refrescat el concepte de composici&#243; funcional amb l'exemple de la memoitzaci&#243;. Quant de potencial...
-    
-    
->>>>>>> origin/Mob
       
-    
-    
+    </p>
+    <p>
       No vull parar.
-    
-    
+    </p>
+    <p>
       
-    
-    
-      Altre detall que apareix a la confe &#233;s el de console.time("sum") i console.timeEnd("sum"). Aquestes ferramentes cronometren una operaci&#243; i mostren el resultat a la consola. Tamb&#233; he refrescat la mem&#242;ria sobre la import&#224;ncia dels costs en les operacions O(x) en els dissenys dels algoritmes.
-    
-    
+    </p>
+    <p>
+      Altre detall que apareix a la confe &#233;s el de console.time(&quot;sum&quot;) i console.timeEnd(&quot;sum&quot;). Aquestes ferramentes cronometren una operaci&#243; i mostren el resultat a la consola. Tamb&#233; he refrescat la mem&#242;ria sobre la import&#224;ncia dels costs en les operacions O(x) en els dissenys dels algoritmes.
+    </p>
+    <p>
       
-<<<<<<< HEAD
+    </p>
+    <p>
+      Ser&#224; convenient accel&#183;lerar amb JS. L'altre dia vaig descobrir un canal que me va sorprendre per la utilitat i lo simp&#224;tic del xaval: &quot;Funfunfunction&quot;. Li farem forat.
+    </p>
+    <p>
+      
     </p>
   </body>
 </html></richcontent>
@@ -72,40 +58,11 @@
 </node>
 </node>
 <node CREATED="1482230586367" ID="ID_179490618" MODIFIED="1482230596252" TEXT="20DIMARTS">
-<node CREATED="1482230761462" ID="ID_1467837663" MODIFIED="1482230782818" TEXT="FETS"/>
-<node CREATED="1482230600277" ID="ID_41936169" MODIFIED="1482230822986" TEXT="OBJECTIUS">
-<node CREATED="1482230843835" ID="ID_437299051" MODIFIED="1482230859144" TEXT="IMMUTABLE.JS"/>
-<node CREATED="1482231420063" ID="ID_1729740259" MODIFIED="1482231487816" TEXT="Text de valoraci&#xf3; i comiat per a Facebook.">
-<node CREATED="1482231529045" ID="ID_825867158" MODIFIED="1482231545641" TEXT="Moltes batalles..."/>
-<node CREATED="1482231615005" ID="ID_492230818" MODIFIED="1482231654556" TEXT="Se senten les cadenes... quan t&apos;alliberes!"/>
-=======
-    
-    
-      Ser&#224; convenient accel&#183;lerar amb JS. L'altre dia vaig descobrir un canal que me va sorprendre per la utilitat i lo simp&#224;tic del xaval: "Funfunfunction". Li farem forat.</pre>
-</richcontent>
-</node>
-</node>
-<node ID="ID_468004220" TEXT="OBJECTIUS" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
-<edge COLOR="#309EFF" />
-<node ID="ID_1270941850" TEXT="Exercicis EXTRA Tuto React" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
-<edge COLOR="#309EFF" />
-<richcontent TYPE="NOTE">
-<pre>Aquests exercicis no resolts tenen molt mala bava. Impliquen canvis profunds en l'estructura i m'est&#224;n apareguent molts problemes il&#183;l&#250;sament inesperats.
-    
-    
-      Array.concat() no fa el que hauria de fer amb l'actualitzaci&#243; de l'estat... en fi. Continue. Porte ja no s&#233; quants dies amb aquesta xorrada... per&#242; suposse que aix&#237; &#233;s com s'apren.</pre>
-</richcontent>
-</node>
-</node>
-</node>
-<node ID="ID_1273146636" TEXT="AUX" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
-<edge COLOR="#309EFF" />
-<node ID="ID_8563047081" TEXT="Idees" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
-<edge COLOR="#309EFF" />
-<node ID="ID_6084357448" TEXT="ACADEMIA SPAWN" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
-<edge COLOR="#309EFF" />
-<richcontent TYPE="NOTE">
-<pre>&#201;s perdre una oportunitat brutal amb un potencial immens si no organitzem els coneixements crucials de forma que puguen ser consumits, aplicats i difosos de la manera m&#233;s efectiva possible.
+<node CREATED="1482230761462" ID="ID_1467837663" MODIFIED="1482230782818" TEXT="FETS">
+<node COLOR="#000034" CREATED="1482327246647" HGAP="350" ID="ID_8563047081" MODIFIED="1482327338920" TEXT="Idees" VSHIFT="-36">
+<edge COLOR="#309eff"/>
+<node COLOR="#000034" CREATED="1482327246647" ID="ID_6084357448" MODIFIED="1482327246647" TEXT="ACADEMIA SPAWN">
+<richcontent TYPE="NOTE"><pre>&#201;s perdre una oportunitat brutal amb un potencial immens si no organitzem els coneixements crucials de forma que puguen ser consumits, aplicats i difosos de la manera m&#233;s efectiva possible.
 
 Toca establir les bases per una nova societat. I les TIC (Tecnologies de la Informaci&#243; i la Comunicaci&#243;) han de ser una base compartida, atresorada i especialment protegida perqu&#232; ser&#224;n les ferramentes que ens garantir&#224;n un progr&#233;s tan necessari com urgent.
 
@@ -123,15 +80,20 @@ Crec que podem endre&#231;ar aquest desastre que hem provocat. No sols &#233;s u
 
 Altre aspecte important ser&#224; la perspectiva espai-temporal per calcular la factibilitat dels projectes que hi sorgisquen. La investigaci&#243; de l'estat actual de cada camp ser&#224; un treball necessari, aix&#237; com la investigaci&#243; de la hist&#242;ria en cada cas. Hem d'aprofitar cada error com&#233;s en el passat i tractar d'evitar i preveure el m&#224;xim possible d'aquells que poden apareixer en l'aplicaci&#243; dels projectes.
 
-La distribuci&#243; de tasques &#233;s un aspecte clau. Les tasques s&#243;n imprescindibles, les persones no. Per tant la circulaci&#243; interna d'integrants pot ser lliure mentre la cartera de tasques segueixca activa i funcional.</pre>
-</richcontent>
+La distribuci&#243; de tasques &#233;s un aspecte clau. Les tasques s&#243;n imprescindibles, les persones no. Per tant la circulaci&#243; interna d'integrants pot ser lliure mentre la cartera de tasques segueixca activa i funcional.</pre></richcontent>
+<edge COLOR="#309eff"/>
 </node>
-<node ID="ID_8823765107" TEXT="La dist&#224;ncia" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
-<edge COLOR="#309EFF" />
-<richcontent TYPE="NOTE">
-<pre>Ens separen abismes plens d'ignor&#224;ncia. Quan acabem amb ella, s'obrir&#224;n tants ponts entre nosaltres que potser arribem a comprendre coses molt profundes. El que s&#237; aconseguirem sense cap mena de dubtes &#233;s millorar la situaci&#243; actual. Trobarem remeis. I una nova era de renaixements comen&#231;ar&#224; per fi.</pre>
-</richcontent>
->>>>>>> origin/Mob
+<node COLOR="#000034" CREATED="1482327246651" ID="ID_8823765107" MODIFIED="1482327246651" TEXT="La dist&#xe0;ncia">
+<richcontent TYPE="NOTE"><pre>Ens separen abismes plens d'ignor&#224;ncia. Quan acabem amb ella, s'obrir&#224;n tants ponts entre nosaltres que potser arribem a comprendre coses molt profundes. El que s&#237; aconseguirem sense cap mena de dubtes &#233;s millorar la situaci&#243; actual. Trobarem remeis. I una nova era de renaixements comen&#231;ar&#224; per fi.</pre></richcontent>
+<edge COLOR="#309eff"/>
+</node>
+</node>
+</node>
+<node CREATED="1482230600277" ID="ID_41936169" MODIFIED="1482230822986" TEXT="OBJECTIUS">
+<node CREATED="1482230843835" ID="ID_437299051" MODIFIED="1482230859144" TEXT="IMMUTABLE.JS"/>
+<node CREATED="1482231420063" ID="ID_1729740259" MODIFIED="1482231487816" TEXT="Text de valoraci&#xf3; i comiat per a Facebook.">
+<node CREATED="1482231529045" ID="ID_825867158" MODIFIED="1482231545641" TEXT="Moltes batalles..."/>
+<node CREATED="1482231615005" ID="ID_492230818" MODIFIED="1482231654556" TEXT="Se senten les cadenes... quan t&apos;alliberes!"/>
 </node>
 </node>
 </node>
