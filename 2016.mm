b13d895c2dbd1c6b9fa61feb90abc34659968fc5
SyBrFlash <sybr@flash>
2016-12-23 17:56:43 +0100

    23again?

--- 2016.mm
+++ 2016.mm
@@ -1,16 +1,17 @@
-<map version="0.9.0">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#000034" CREATED="1482341243315" ID="ID_29907350" MODIFIED="1482341243315" TEXT="2016">
-<edge COLOR="#309eff"/>
-<font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#000034" CREATED="1482341243315" ID="ID_1391873210" MODIFIED="1482341243315" POSITION="right" TEXT="DESEMBRE">
-<edge COLOR="#309eff"/>
-<node COLOR="#000034" CREATED="1482341243316" ID="ID_1096773859" MODIFIED="1482341243316" TEXT="19DILLUNS">
-<edge COLOR="#309eff"/>
-<node COLOR="#000034" CREATED="1482341243316" ID="ID_254155864" MODIFIED="1482341243316" TEXT="FETS">
-<edge COLOR="#309eff"/>
-<node COLOR="#000034" CREATED="1482341243316" ID="ID_1261535768" MODIFIED="1482341243316" TEXT="Coses apreses">
-<richcontent TYPE="NOTE"><pre>No hem acabat els exercicis Extra... com era d'esperar, per&#242; he apr&#233;s algunes coses noves. I &#233;s clar que si vaig a gastar JS de forma seriosa, haur&#233; de dominar-lo molt m&#233;s del que ho faig ara. I necessitar&#233; fer-ho amb bon nivell de detall. &#201;s per aix&#242; que m'he encabotat en fer funcionar aquests exercicis. Pel cam&#237; he anat aprenent sobre Map, Slice, Reduce i altres funcions &#250;tils de JS. Avu&#237; he descobert Object.is() i he comen&#231;at a investigar Immutable.js. Sembla interessant. Vegent la confer&#232;ncia de presentaci&#243; de Lee Byron a OREDEV, m'ha refrescat el concepte de composici&#243; funcional amb l'exemple de la memoitzaci&#243;. Quant de potencial...
+<map version="0.9.0" NAME="2016" DATE="1482512010000" FILE="2016.mm" MAPID="3UmrgsnMQlG667mEOnk18g==" BGCOLOR="#F2F3F7">
+<node ID="ID_29907350" TEXT="2016" COLOR="#000034" TSGSTYLE="NodeMasterGraphic">
+<font NAME="SansSerif" SIZE="16" />
+<edge COLOR="#309EFF" />
+<node ID="ID_1391873210" TEXT="DESEMBRE" POSITION="right" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<node ID="ID_1096773859" TEXT="19DILLUNS" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<node ID="ID_254155864" TEXT="FETS" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<node ID="ID_1261535768" TEXT="Coses apreses" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<richcontent TYPE="NOTE">
+<pre>No hem acabat els exercicis Extra... com era d'esperar, per&#242; he apr&#233;s algunes coses noves. I &#233;s clar que si vaig a gastar JS de forma seriosa, haur&#233; de dominar-lo molt m&#233;s del que ho faig ara. I necessitar&#233; fer-ho amb bon nivell de detall. &#201;s per aix&#242; que m'he encabotat en fer funcionar aquests exercicis. Pel cam&#237; he anat aprenent sobre Map, Slice, Reduce i altres funcions &#250;tils de JS. Avu&#237; he descobert Object.is() i he comen&#231;at a investigar Immutable.js. Sembla interessant. Vegent la confer&#232;ncia de presentaci&#243; de Lee Byron a OREDEV, m'ha refrescat el concepte de composici&#243; funcional amb l'exemple de la memoitzaci&#243;. Quant de potencial...
     
     
       
@@ -28,30 +29,34 @@
       
     
     
-      Ser&#224; convenient accel&#183;lerar amb JS. L'altre dia vaig descobrir un canal que me va sorprendre per la utilitat i lo simp&#224;tic del xaval: "Funfunfunction". Li farem forat.</pre></richcontent>
-<edge COLOR="#309eff"/>
+      Ser&#224; convenient accel&#183;lerar amb JS. L'altre dia vaig descobrir un canal que me va sorprendre per la utilitat i lo simp&#224;tic del xaval: "Funfunfunction". Li farem forat.</pre>
+</richcontent>
 </node>
 </node>
-<node COLOR="#000034" CREATED="1482341243319" ID="ID_468004220" MODIFIED="1482341243319" TEXT="OBJECTIUS">
-<edge COLOR="#309eff"/>
-<arrowlink DESTINATION="ID_41936169" ENDARROW="Default" ENDINCLINATION="22;0;" ID="Arrow_ID_715583388" STARTARROW="None" STARTINCLINATION="22;0;"/>
-<node COLOR="#000034" CREATED="1482341243322" ID="ID_1270941850" MODIFIED="1482341243322" TEXT="Exercicis EXTRA Tuto React">
-<richcontent TYPE="NOTE"><pre>Aquests exercicis no resolts tenen molt mala bava. Impliquen canvis profunds en l'estructura i m'est&#224;n apareguent molts problemes il&#183;l&#250;sament inesperats.
+<node ID="ID_468004220" TEXT="OBJECTIUS" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<arrowlink ARROW_ID="ID_1456632374" DESTINATION="ID_41936169" STARTINCLINATION="22;0;" ENDINCLINATION="22;0;" STARTARROW="None" ENDARROW="Default" />
+<node ID="ID_1270941850" TEXT="Exercicis EXTRA Tuto React" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<richcontent TYPE="NOTE">
+<pre>Aquests exercicis no resolts tenen molt mala bava. Impliquen canvis profunds en l'estructura i m'est&#224;n apareguent molts problemes il&#183;l&#250;sament inesperats.
     
     
-      Array.concat() no fa el que hauria de fer amb l'actualitzaci&#243; de l'estat... en fi. Continue. Porte ja no s&#233; quants dies amb aquesta xorrada... per&#242; suposse que aix&#237; &#233;s com s'apren.</pre></richcontent>
-<edge COLOR="#309eff"/>
+      Array.concat() no fa el que hauria de fer amb l'actualitzaci&#243; de l'estat... en fi. Continue. Porte ja no s&#233; quants dies amb aquesta xorrada... per&#242; suposse que aix&#237; &#233;s com s'apren.</pre>
+</richcontent>
 </node>
 </node>
 </node>
-<node COLOR="#000034" CREATED="1482341243323" ID="ID_179490618" MODIFIED="1482341243323" TEXT="20DIMARTS">
-<edge COLOR="#309eff"/>
-<node COLOR="#000034" CREATED="1482341243323" ID="ID_1467837663" MODIFIED="1482341243323" TEXT="FETS">
-<edge COLOR="#309eff"/>
-<node COLOR="#000034" CREATED="1482341243323" ID="ID_8563047081" MODIFIED="1482341243323" TEXT="Idees">
-<edge COLOR="#309eff"/>
-<node COLOR="#000034" CREATED="1482341243324" ID="ID_6084357448" MODIFIED="1482341243324" TEXT="ACADEMIA SPAWN">
-<richcontent TYPE="NOTE"><pre>&#201;s perdre una oportunitat brutal amb un potencial immens si no organitzem els coneixements crucials de forma que puguen ser consumits, aplicats i difosos de la manera m&#233;s efectiva possible.
+<node ID="ID_179490618" TEXT="20DIMARTS" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<node ID="ID_1467837663" TEXT="FETS" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<node ID="ID_8563047081" TEXT="Idees" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<node ID="ID_6084357448" TEXT="ACADEMIA SPAWN" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<richcontent TYPE="NOTE">
+<pre>&#201;s perdre una oportunitat brutal amb un potencial immens si no organitzem els coneixements crucials de forma que puguen ser consumits, aplicats i difosos de la manera m&#233;s efectiva possible.
 
 Toca establir les bases per una nova societat. I les TIC (Tecnologies de la Informaci&#243; i la Comunicaci&#243;) han de ser una base compartida, atresorada i especialment protegida perqu&#232; ser&#224;n les ferramentes que ens garantir&#224;n un progr&#233;s tan necessari com urgent.
 
@@ -69,105 +74,84 @@ Crec que podem endre&#231;ar aquest desastre que hem provocat. No sols &#233;s u
 
 Altre aspecte important ser&#224; la perspectiva espai-temporal per calcular la factibilitat dels projectes que hi sorgisquen. La investigaci&#243; de l'estat actual de cada camp ser&#224; un treball necessari, aix&#237; com la investigaci&#243; de la hist&#242;ria en cada cas. Hem d'aprofitar cada error com&#233;s en el passat i tractar d'evitar i preveure el m&#224;xim possible d'aquells que poden apareixer en l'aplicaci&#243; dels projectes.
 
-La distribuci&#243; de tasques &#233;s un aspecte clau. Les tasques s&#243;n imprescindibles, les persones no. Per tant la circulaci&#243; interna d'integrants pot ser lliure mentre la cartera de tasques segueixca activa i funcional.</pre></richcontent>
-<edge COLOR="#309eff"/>
+La distribuci&#243; de tasques &#233;s un aspecte clau. Les tasques s&#243;n imprescindibles, les persones no. Per tant la circulaci&#243; interna d'integrants pot ser lliure mentre la cartera de tasques segueixca activa i funcional.</pre>
+</richcontent>
 </node>
-<node COLOR="#000034" CREATED="1482341243328" ID="ID_8823765107" MODIFIED="1482341243328" TEXT="La dist&#xe0;ncia">
-<richcontent TYPE="NOTE"><pre>Ens separen abismes plens d'ignor&#224;ncia. Quan acabem amb ella, s'obrir&#224;n tants ponts entre nosaltres que potser arribem a comprendre coses molt profundes. El que s&#237; aconseguirem sense cap mena de dubtes &#233;s millorar la situaci&#243; actual. Trobarem remeis. I una nova era de renaixements comen&#231;ar&#224; per fi.</pre></richcontent>
-<edge COLOR="#309eff"/>
-</node>
-</node>
-</node>
-<node COLOR="#000034" CREATED="1482341243329" ID="ID_41936169" MODIFIED="1482341243329" TEXT="OBJECTIUS">
-<edge COLOR="#309eff"/>
-<arrowlink DESTINATION="ID_0888276761" ENDARROW="Default" ENDINCLINATION="56;-47;" ID="Arrow_ID_1045730191" STARTARROW="None" STARTINCLINATION="15;13;"/>
-<node COLOR="#000034" CREATED="1482341243330" ID="ID_437299051" MODIFIED="1482341243330" TEXT="IMMUTABLE.JS">
-<edge COLOR="#309eff"/>
-</node>
-<node COLOR="#000034" CREATED="1482341243330" ID="ID_1729740259" MODIFIED="1482341243330" TEXT="Text de valoraci&#xf3; i comiat per a Facebook.">
-<edge COLOR="#309eff"/>
-<node COLOR="#000034" CREATED="1482341243331" ID="ID_825867158" MODIFIED="1482341243331" TEXT="Moltes batalles...">
-<edge COLOR="#309eff"/>
-</node>
-<node COLOR="#000034" CREATED="1482341243331" ID="ID_492230818" MODIFIED="1482341243331" TEXT="Se senten les cadenes... quan t&apos;alliberes!">
-<edge COLOR="#309eff"/>
+<node ID="ID_8823765107" TEXT="La dist&#224;ncia" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<richcontent TYPE="NOTE">
+<pre>Ens separen abismes plens d'ignor&#224;ncia. Quan acabem amb ella, s'obrir&#224;n tants ponts entre nosaltres que potser arribem a comprendre coses molt profundes. El que s&#237; aconseguirem sense cap mena de dubtes &#233;s millorar la situaci&#243; actual. Trobarem remeis. I una nova era de renaixements comen&#231;ar&#224; per fi.</pre>
+</richcontent>
 </node>
 </node>
 </node>
+<node ID="ID_41936169" TEXT="OBJECTIUS" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<arrowlink ARROW_ID="ID_4877714473" DESTINATION="ID_0888276761" STARTINCLINATION="15;13;" ENDINCLINATION="56;-47;" STARTARROW="None" ENDARROW="Default" />
+<node ID="ID_437299051" TEXT="IMMUTABLE.JS" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
 </node>
-<node COLOR="#000034" CREATED="1482341243331" FOLDED="true" ID="ID_4647375461" MODIFIED="1482420038296" TEXT="21DIMECRES">
-<richcontent TYPE="NOTE"><html>
-  <head>
+<node ID="ID_1729740259" TEXT="Text de valoraci&#243; i comiat per a Facebook." COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<node ID="ID_825867158" TEXT="Moltes batalles..." COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+</node>
+<node ID="ID_492230818" TEXT="Se senten les cadenes... quan t'alliberes!" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+</node>
+</node>
+</node>
+</node>
+<node ID="ID_4647375461" TEXT="21DIMECRES" COLOR="#000034" TSGSTYLE="NodeTextGraphic" FOLDED="true">
+<edge COLOR="#309EFF" />
+<richcontent TYPE="NOTE">
+<pre>Sembla que tot me pesa massa. Per&#242; vaig organitzant-me... Veig que roda... lent&#237;ssim per&#242; roda. Digam que &#233;s molta cosa la que &#233;s arrastrada... ANEM LLUNY! I a&#231;&#242; que estem constru&#239;nt amb tant d'esfor&#231; i sacrifici &#233;s IMPORTANT&#205;SSIM! Fem tard. Per&#242; aix&#242; HA PASSAT SEMPRE. Sabent-ho ara, caldr&#224; afian&#231;ar la dilig&#232;ncia i guanyar in&#232;rcia. Estic m&#233;s que segur que el principi de la in&#232;rcia &#233;s tamb&#233; aplicable ac&#237;. AVANT!</pre>
+</richcontent>
+<node ID="ID_0888276761" TEXT="OBJECTIUS" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<richcontent TYPE="NOTE">
+<pre>Seguim amb IMMUTABLE.</pre>
+</richcontent>
+</node>
+<node ID="ID_1284694766" TEXT="Amb la dist&#224;ncia" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<richcontent TYPE="NOTE">
+<pre>&#201;s amb la dist&#224;ncia que es perd la identitat. Es conf&#243;n i es revela c&#243;m &#233;s, compartida i difosa.</pre>
+</richcontent>
+</node>
+</node>
+<node ID="ID_1715688645" TEXT="22DIJOUS" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<node ID="ID_1278310819" TEXT="TEXT" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<node ID="ID_1146121884" TEXT="IMMUTABILITAT" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<richcontent TYPE="NOTE">
+<pre>Buscant el terme "immutable" a internet ha aparegut un enlla&#231; on es defensava la immutabilitat de D&#233;u; que &#233;s lo &#250;nic que ho &#233;s.
     
-  </head>
-  <body>
-    <p>
-      Sembla que tot me pesa massa. Per&#242; vaig organitzant-me... Veig que roda... lent&#237;ssim per&#242; roda. Digam que &#233;s molta cosa la que &#233;s arrastrada... ANEM LLUNY! I a&#231;&#242; que estem constru&#239;nt amb tant d'esfor&#231; i sacrifici &#233;s IMPORTANT&#205;SSIM! Fem tard. Per&#242; aix&#242; HA PASSAT SEMPRE. Sabent-ho ara, caldr&#224; afian&#231;ar la dilig&#232;ncia i guanyar in&#232;rcia. Estic m&#233;s que segur que el principi de la in&#232;rcia &#233;s tamb&#233; aplicable ac&#237;. AVANT!
-    </p>
-  </body>
-</html></richcontent>
-<edge COLOR="#309eff"/>
-<node COLOR="#000034" CREATED="1482341243332" ID="ID_0888276761" MODIFIED="1482341243332" TEXT="OBJECTIUS">
-<richcontent TYPE="NOTE"><pre>Seguim amb IMMUTABLE.</pre></richcontent>
-<edge COLOR="#309eff"/>
-</node>
-<node CREATED="1482365904351" ID="ID_1284694766" MODIFIED="1482366063868" TEXT="Amb la dist&#xe0;ncia">
-<richcontent TYPE="NOTE"><html>
-  <head>
     
-  </head>
-  <body>
-    <p>
-      &#201;s amb la dist&#224;ncia que es perd la identitat. Es conf&#243;n i es revela c&#243;m &#233;s, compartida i difosa.
-    </p>
-  </body>
-</html></richcontent>
-</node>
-</node>
-<node CREATED="1482420038932" ID="ID_1715688645" MODIFIED="1482420044883" TEXT="22DIJOUS">
-<node CREATED="1482420053002" ID="ID_1278310819" MODIFIED="1482420065947" TEXT="TEXT">
-<node CREATED="1482420069007" ID="ID_1146121884" MODIFIED="1482421680472" TEXT="IMMUTABILITAT">
-<richcontent TYPE="NOTE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      Buscant el terme &quot;immutable&quot; a internet ha aparegut un enlla&#231; on es defensava la immutabilitat de D&#233;u; que &#233;s lo &#250;nic que ho &#233;s.
-    </p>
-    <p>
       
-    </p>
-    <p>
-      Si te poses, pots trobar una dualitat a cada aspecte de la vida, dos punts de vista completament oposats per&#242; complementaris. El primer i el m&#233;s f&#224;cil d'entendre, &#233;s el teu, el que tens des de la teua pr&#242;pia perspectiva com a individu. El 'jo' mateix. L'altre, seria el 'no-jo'. Una mena d'imatge especular a la inversa del 'jo' que tamb&#233;, podria obeir, segons se'l rebusque a una individualitat inc&#242;moda... incomprensible, metaf&#237;sica i, en ess&#232;ncia aliena a t&#250; mateix. Seria sospitable que aquesta perspectiva f&#243;ra compartida per cada individu... o no.
-    </p>
-  </body>
-</html></richcontent>
-</node>
-</node>
-</node>
-<node CREATED="1482510834883" ID="ID_450700878" MODIFIED="1482510841436" TEXT="23DIVENDRES">
-<node CREATED="1482510849864" ID="ID_516698692" MODIFIED="1482510856874" TEXT="TEXT">
-<node CREATED="1482510860106" ID="ID_1594042481" MODIFIED="1482511368675" TEXT="Llenguatge Universal">
-<richcontent TYPE="NOTE"><html>
-  <head>
     
-  </head>
-  <body>
-    <p>
-      Us sona la teoria de que si una civilitzaci&#243; intel&#183;ligent intent&#233;s posar-se en contacte amb nosaltres ho faria mitjan&#231;ant el llenguatge universal de la ci&#232;ncia... les matem&#224;tiques, no &#233;s nova... per contra, no sembla haver-se tingut massa en compte. HOLA PLANTES!... Fractals? Hmm... en serio!? O ADN! Per&#242; c&#243;m pot ser possible!??? Molt queda per descobrir, per&#242; sobretot per assimilar i contextualitzar! Amb tant que sabem, tenim un nivell educatiu est&#224;ndar deshonrosament baix.
-    </p>
-    <p>
+    
+      Si te poses, pots trobar una dualitat a cada aspecte de la vida, dos punts de vista completament oposats per&#242; complementaris. El primer i el m&#233;s f&#224;cil d'entendre, &#233;s el teu, el que tens des de la teua pr&#242;pia perspectiva com a individu. El 'jo' mateix. L'altre, seria el 'no-jo'. Una mena d'imatge especular a la inversa del 'jo' que tamb&#233;, podria obeir, segons se'l rebusque a una individualitat inc&#242;moda... incomprensible, metaf&#237;sica i, en ess&#232;ncia aliena a t&#250; mateix. Seria sospitable que aquesta perspectiva f&#243;ra compartida per cada individu... o no.</pre>
+</richcontent>
+</node>
+</node>
+</node>
+<node ID="ID_450700878" TEXT="23DIVENDRES" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<node ID="ID_516698692" TEXT="TEXT" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<node ID="ID_1594042481" TEXT="Llenguatge Universal" COLOR="#000034" TSGSTYLE="NodeTextGraphic">
+<edge COLOR="#309EFF" />
+<richcontent TYPE="NOTE">
+<pre>Us sona la teoria de que si una civilitzaci&#243; intel&#183;ligent intent&#233;s posar-se en contacte amb nosaltres ho faria mitjan&#231;ant el llenguatge universal de la ci&#232;ncia... les matem&#224;tiques, no &#233;s nova... per contra, no sembla haver-se tingut massa en compte. HOLA PLANTES!... Fractals? Hmm... en serio!? O ADN! Per&#242; c&#243;m pot ser possible!??? Molt queda per descobrir, per&#242; sobretot per assimilar i contextualitzar! Amb tant que sabem, tenim un nivell educatiu est&#224;ndar deshonrosament baix.
+    
+    
       
-    </p>
-    <p>
-      Toca posar-li remei.
-    </p>
-    <p>
-      
-    </p>
-  </body>
-</html>
+    
+    
+      Toca posar-li remei.</pre>
 </richcontent>
 </node>
 </node>
