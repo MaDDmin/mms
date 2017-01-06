e7d9fbfcdea071e40d79f17ba8acda4a50f165ac
SyBr <sybr@sybrcq>
2017-01-06 18:49:57 +0100

    Nou map, per al 2017

---
+++ 2017.mm
@@ -0,0 +1,62 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1483559466773" ID="ID_513056124" MODIFIED="1483723598311" TEXT="2017">
+<node CREATED="1483559479388" FOLDED="true" ID="ID_1287889480" MODIFIED="1483723600272" POSITION="right" TEXT="4">
+<node CREATED="1483559486098" ID="ID_89306903" MODIFIED="1483573503009" TEXT="Text">
+<node CREATED="1483559496133" ID="ID_1036955585" MODIFIED="1483559612191" TEXT="El que hi ha">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Aix&#242; &#233;s el que hi ha. Un mar de coneixements per a qui el vullga... amb una infinitat de possibilitats per a qui les puga!
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1483573505678" ID="ID_449137394" MODIFIED="1483574337917" TEXT="Me costa de creure">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Avui, mon pare ha portat l'&#218;nic a l'hospital veterinari de Silla. M'ha dit d'anar i he rebutjat perqu&#232; no vull hist&#242;ries amb metges ni, per extensi&#243;, amb veterinaris. El gos est&#224; mal. Sagna de vegades per la uretra i estava perdent la for&#231;a que el caracteritzava. L'ha deixat all&#237;. I no m'ha agradat gens. L'animalet se sentir&#224; abandonat altra vegada! No m'ho puc explicar. C&#243;m pot alg&#250; fer aix&#242; i quedar-se tranquil? Soc molt recel&#243;s amb els metges i no descarte que el mediquen quan els resultats dels an&#224;lisis s&#243;n, segons m'ha dit, greus. No s&#233; el que durar&#224; el pobre gos, per&#242; no puc quedar-me tranquil pensant c&#243;m estar&#224; sentint-se de desolat. &#201;s un colp que no s&#233; si podr&#224; encaixar, donada la situaci&#243;. Sols espere que aguante fins dem&#224; a les 18. M'he estat pensant d'anar de bon mat&#237; per fer-li comprendre que no est&#224; sol i que no l'hem abandonat. Pot ser siga un factor decisiu en la seua situaci&#243;. No s&#233; qu&#232; fer.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1483723601827" ID="ID_394114295" MODIFIED="1483723611952" POSITION="right" TEXT="6">
+<node CREATED="1483723614064" ID="ID_1112581523" MODIFIED="1483723616674" TEXT="Text">
+<node CREATED="1483723619493" ID="ID_1994810931" MODIFIED="1483724222402" TEXT="I l&apos;animalet encara all&#xed;, tancat.">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      He dit ja que me costa de creure?
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Efectivament, &#218;nic encara est&#224; all&#224;, tancat, a l'hospital veterinari de Silla. No ens ha de passar m&#233;s. El pobre animalet, tot a&#239;llat en quant no tenen ni la m&#233;s remota idea del que li passa.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Molt mal. El vull ja a casa. Estiga b&#233; ja. Si s'ha de morir, que se muiga tranquil, en casa, entre nosaltres. Me fan dubtar si &#233;s pitjor el remei que la pr&#242;pia enfermetat, literalment. Si al menys vegeres que saben el qu&#232; fan... per&#242; s'aferren a mantindre'l tancat encara que no &#233;s en absolut necessari. En principi el porten ara. Veurem.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
