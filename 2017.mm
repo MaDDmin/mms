d43e8aca4c03a00e212646f3d929d8f518b01890
SyBr <sybr@sybrcq>
2017-01-09 15:10:50 +0100

    2017 9 La roba.

--- 2017.mm
+++ 2017.mm
@@ -1,7 +1,7 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1483559466773" ID="ID_513056124" MODIFIED="1483723598311" TEXT="2017">
-<node CREATED="1483559479388" FOLDED="true" ID="ID_1287889480" MODIFIED="1483723600272" POSITION="right" TEXT="4">
+<node CREATED="1483559479388" ID="ID_1287889480" MODIFIED="1483737731551" POSITION="right" TEXT="4">
 <node CREATED="1483559486098" ID="ID_89306903" MODIFIED="1483573503009" TEXT="Text">
 <node CREATED="1483559496133" ID="ID_1036955585" MODIFIED="1483559612191" TEXT="El que hi ha">
 <richcontent TYPE="NOTE"><html>
@@ -29,7 +29,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1483723601827" ID="ID_394114295" MODIFIED="1483723611952" POSITION="right" TEXT="6">
+<node CREATED="1483723601827" ID="ID_394114295" MODIFIED="1483815323880" POSITION="right" TEXT="6">
 <node CREATED="1483723614064" ID="ID_1112581523" MODIFIED="1483723616674" TEXT="Text">
 <node CREATED="1483723619493" ID="ID_1994810931" MODIFIED="1483734273590" TEXT="I l&apos;animalet encara all&#xed;, tancat.">
 <richcontent TYPE="NOTE"><html>
@@ -71,8 +71,54 @@
       Ve tot del mateix lloc. No hauriem de passar per aquesta situaci&#243; en una societat realment avan&#231;ada i lliure. Per qu&#232; dic ara a&#231;&#242;? Doncs &#233;s clar, <b>un servei com aquest hauria de ser gestionat p&#250;blicament, en obert, amb informaci&#243; oberta a opinions, a participaci&#243;...</b>&#160;Molt b&#233;. Moltes idees. Una altra per a la col&#183;lecci&#243;. Veurem si alguna veu la llum.
     </p>
   </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1483816187821" ID="ID_331390983" MODIFIED="1483970671133" POSITION="right" TEXT="7">
+<node CREATED="1483816206330" ID="ID_592285899" MODIFIED="1483816211645" TEXT="Text">
+<node CREATED="1483816211647" ID="ID_396999768" MODIFIED="1483816553628" TEXT="&#xda;nic ja est&#xe0; en casa.">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ja est&#224; en casa, per&#242; amb ell han vingut les idees i les formes d'actuar dels veterinaris... Jo pense que amb un canvi de dieta, una sana, amb pinso vegetal... an&#224;vem a veure millories de seguida. &#201;s tot un tren d'infortunis. Res de nou. Pel que me toca tinc molt per aclarir i avan&#231;ar i aix&#242; s&#243;n coses que no far&#224; ning&#250; per mi.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Per tant... <b>Toca posar-se en marxa!</b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1483970672263" ID="ID_130232545" MODIFIED="1483970678386" POSITION="right" TEXT="9">
+<node CREATED="1483970683437" ID="ID_1136337140" MODIFIED="1483970744038" TEXT="Problemes de FLOWs">
+<node CREATED="1483970753978" FOLDED="true" ID="ID_1389961661" MODIFIED="1483970997578" TEXT="La roba">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#201;s un problema perqu&#232; ocupa espai i s'utilitza cont&#237;nuament, al menys, en dos conjunts diferents. Tendeix a ser acumulativa i renovar-la, sovint &#233;s algo problem&#224;tic.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Cal un <b>sistema fiable de gesti&#243; de residus</b>. Nova idea per a la col&#183;lecci&#243;? No. Aquesta no &#233;s nova. Per&#242; s&#237;, tamb&#233; est&#224; per fer.
+    </p>
+  </body>
 </html>
 </richcontent>
+<node CREATED="1483970766440" ID="ID_1447617504" MODIFIED="1483970772813" TEXT="Estiu"/>
+<node CREATED="1483970774607" ID="ID_398691332" MODIFIED="1483970781432" TEXT="Hivern"/>
 </node>
 </node>
 </node>
